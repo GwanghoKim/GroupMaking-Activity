4465cc6e768958e2201eb53b3d10a08a85da643c
Gwangho Kim <kkh01451@github.com>
2021-07-31 17:09:24 +0900

    start
    시작

---
+++ GroupMaking&Activity/GroupMaking&Activity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupMaking&Activity
+//
+//  Created by Gwang Ho Kim on 2021/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
